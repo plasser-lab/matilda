35f6119f92b8cfd198aeffd933f632f311ae16e4
Giulia Woelfle-Conway <ntgw2@lnx-w243-3.lunet.lboro.ac.uk>
2026-03-20 14:30:07 +0000

    Improved user interface

--- scripts/fc_mini.py
+++ scripts/fc_mini.py
@@ -14,13 +14,13 @@ usage: Spectrum using Franck-Condon progression formula based on Huang-Rhys fact
 class fc_options(input_options.write_options):
     def fc_input(self):
         
-        self.choose_list("Select mode:", "abs_emi", [ (1, "Absorption"), (2, "Emission")], 1)
-        self.read_float("Electronic adiabatic energy (in Hartree): ", "dEH", 1.0)
-        self.read_float("Oscillator strength (dimensionless): ", "f", 1.0)
-        self.read_float("w_min(in cm^-1): ", "w_min", 200.0)
-        self.read_float("S_min: ", "S_min", 0.1)
+        self.choose_list("Select mode", "abs_emi", [ (1, "Absorption"), (2, "Emission")], 1)
+        self.read_float("Electronic adiabatic energy in Hartree", "dEH", 1.0)
+        self.read_float("Oscillator strength", "f", 1.0)
+        self.read_float("Minimum vibrational frequency in cm^-1", "w_min", 200.0)
+        self.read_float("Minimum Huang-Rhys factor value", "S_min", 0.1)
         self.read_yn("Show stick spectrum overlay", "plot_sticks", False)
-        self.read_float("Number of points in spectrum", "npoints", 2000)
+        self.read_int("Number of points in spectrum", "npoints", 2000)
     
         if self["abs_emi"] == 1:
             self.read_yn("Assume omega/omega_I0 = 1 sharp lineshape approximation for the absorption cross-section?", "use_omega_omegaI0", True)   
@@ -34,14 +34,12 @@ class fc_options(input_options.write_options):
         hr = lib_fc.HRData(self, hr_data_file)
         hr.run_plot()
 
-
-
 if __name__=='__main__':
     matilda_header.print_header('FCmini - Franck-Condon simulations')
 
     if len(sys.argv) < 2:
         print("Usage: fc_mini_v2.py <hr_data_file>")
-        raise error_handler.MsgError('Specify Huang Rhys data file')
+        raise error_handler.MsgError('Specify Huang-Rhys data file')
 
     hr_data_file = sys.argv[1]
 
